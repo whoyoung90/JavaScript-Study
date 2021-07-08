50fe8373fddb490ff6683bf36ba5b4965fdc1f05
wooyoungkim <whoyoung90@naver.com>
2021-07-08 16:04:26 +0900

    add: object 복습 완료

--- src/Components/Objects.js
+++ src/Components/Objects.js
@@ -1,4 +1,3 @@
-import { buildQueries } from '@testing-library/react';
 import React from 'react';
 
 export const Objects = () => {
@@ -21,28 +20,26 @@ export const Objects = () => {
   // 1. Literals and properties
   const obj1 = {}; // 'object literal' syntax
   const obj2 = new Object(); // 'object constructor' syntax | Object에 정의된 constructor가 호출된다. | class란 template을 이용해서 만드는 방법.
+  console.log(obj1, obj2);
+
   function print(person) {
     console.log(person.name);
     console.log(person.age);
   }
-
   const foo = { name: 'Jack', age: 32 };
   print(foo);
 
   // with JavaScript magic (dynamically typed language)
   // can add properties later
   foo.hasJob = true;
-  console.log(foo.hasJob);
+  console.log(foo);
 
   // can delete properties later
   delete foo.hasJob;
-  console.log(foo.hasJob);
+  console.log(foo);
 
   // 2. Computed properties
   // key should be always string
-  // 정확하게 어떤 key가 필요한지 모를 때!
-  // 실시간으로 원하는 key의 값을 받아오고 싶을 때(runtime에서 결정될 때) 사용!
-  console.log(foo['name']);
 
   function printValue(obj, key) {
     // console.log(obj.key); // object에 key라는 프로퍼티가 당장 없으니까 undefined
@@ -52,41 +49,44 @@ export const Objects = () => {
   printValue(foo, 'age');
 
   // 3. Property value shorthand
-  const person1 = { name: 'bob', age: 2 };
-  const person2 = { name: 'steve', age: 3 };
-  const person3 = new Person('dave', 4); // JavaScript 엔진이 알아서 object 생성
+  const person1 = { name: 'Mikasa', age: 1 };
+  const person2 = { name: 'libai', age: 2 };
+  const person3 = new SurveyCorp('Ackerman', 3); // JavaScript 엔진이 알아서 object 생성
   console.log(person3);
 
   // 4. Constructor function
-  function Person(name, age) {
+  function SurveyCorp(name, age) {
     // this = {}; 생략
     this.name = name;
     this.age = age;
-    // return this 생략
   }
 
   // 5. in operator: property existence check (key in obj)
   console.log('name' in foo);
-  console.log('nono' in foo);
+  console.log('none' in foo);
 
   // 6. for..in vs for..of
   // for (key in obj)
   for (let key in foo) {
-    console.log(key);
+    console.log(key); // name, age
+    console.log(foo[key]); //jack, 32
   }
 
   // for (value of iterable)
-  const array = [1, 2, 4, 5];
+  const array = [7, 8, 9];
+  for (let value in array) {
+    console.log(value); // 0, 1, 2
+  }
   for (let value of array) {
-    console.log(value);
+    console.log(value); //7, 8, 9
   }
 
   // 7. Fun cloning
   // Object.assign(dest, [obj1, obj2, obj3...])
-  const user = { name: 'Eren', age: '20' };
+  const user = { name: 'Eren', age: 20 };
   const user2 = user;
   user2.name = 'Yegar';
-  console.log(user); // 바라보는 ref가 같으니까!
+  console.log('user', user); // 바라보는 ref가 같으니까!
 
   // old way
   const user3 = {};
@@ -100,15 +100,50 @@ export const Objects = () => {
   console.log('user4', user4);
 
   // another example
-  const fruit1 = { color: 'red' };
-  const fruit2 = { color: 'blue', size: 'big' };
-  const mixed = Object.assign({}, fruit1, fruit2); //중복되면 최신 값을 덮어씌움
+  const fruit1 = { color: 'red', size: 'big' };
+  const fruit2 = { color: 'blue' };
+  const mixed = Object.assign({}, fruit1, fruit2); //중복되면 최신 값 overwriting
   console.log('mixed', mixed);
 
   return (
     <>
-      <div>Object</div>
-      <div>Object.assign : JavaScript Object는 언제나 상속할 수 있다!</div>
+      <h2>Object</h2>
+      <div>
+        <h2>✅ foo['name']</h2>
+        <ul>
+          <li>정확하게 어떤 key가 필요한지 모를 때!</li>
+          <li>
+            실시간으로 원하는 key값을 받아오고 싶을 때(runtime에서 결정될 때)
+            사용!
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>✅ ES6 Class 이전에는? Constructor function</h2>
+        <ul>
+          <li>클래스를 정의하지 않고 바로 Object를 만듬</li>
+          <li>function을 이용한 template작성</li>
+          <li>
+            This constructor function may be converted to a class declaration.
+            ts(80002)
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>✅ for..in vs for..of [7, 8, 9] </h2>
+        <ul>
+          <li>for (key in obj) 👉 key값 추출 0, 1, 2</li>
+          <li>for (value of iterable) 👉 value값 추출 7, 8, 9</li>
+          <li>
+            Object는 for..in만 가능 (not iterable) <br /> Array는 for..in
+            for..of 둘다 가능
+          </li>
+        </ul>
+      </div>
+
+      <h4>Object.assign : JavaScript Object는 언제나 상속할 수 있다!</h4>
     </>
   );
 };
